5689e9e8dd878636df5db81ed93fa34d2a2953f0
student <mpneff19@gmail.com>
2018-08-02 10:37:33 -0600

    restyled the mainpage with a bit of component breakdown

---
+++ src/components/formFields.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+
+export class PlayerImage extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.id = this.props.playerID
+
+    this.handleSelectedImage = this.handleSelectedImage.bind(this);
+  }
+
+  handleSelectedImage = function(event) {
+    let image = document.getElementById(`player-${this.id}-image`);
+    image.src = URL.createObjectURL(event.target.files[0]);
+  }
+
+  render() {
+    const {input, imageUrl} = this.props;
+    return (
+      <div className={`playerImageWrapper`}>
+        <img
+          id={`player-${this.id}-image`}
+          className='playerImage'
+          src={imageUrl}
+        />
+        <input
+          className='form-image__replace'
+          type='button'
+          value='Replace'
+          onClick={
+            () => document.getElementById(`file${this.id}`) ? document.getElementById(`file${this.id}`).click() : ''
+          }
+        />
+        <input {...input}
+          type='file'
+          style={{display: 'none'}}
+          id={`file${this.id}`}
+          accepts='image/*'
+          value={undefined}
+          onChange={this.handleSelectedImage}
+        />
+      </div>
+    )
+  }
+}
--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react';3
 
+import {PlayerImage} from './formFields';
 
 class MainPage extends Component {
 
@@ -12,23 +13,27 @@ class MainPage extends Component {
         <div className='mainpage_container_1'>
           <div className='playerArea'>
             <div className='playerArea_imageHolder'>
-              <img className='playerImage' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
-              <img className='cardImage' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+              <PlayerImage
+              type='file'
+              playerID = '1'
+              imageUrl = 'https://placecage.com/gif/200/200'
+              />
+              <img className='cardImage' src={"https://placecage.com/gif/700/800"}/>
             </div>
             <div className='playerArea_nameHolder' id='player1Name'>
               <div className='playerArea_nameHolder_name'>Test</div>
 
               <div className='chipArea'>
                 <div className='chip'>
-                  <img  id='p1Chip1' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                  <img  id='p1Chip1' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
                 <div className='chip'>
-                  <img  id='p1Chip2' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                  <img  id='p1Chip2' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
                 <div className='chip'>
-                  <img  id='p1Chip3' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                  <img  id='p1Chip3' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
               </div>
@@ -39,23 +44,27 @@ class MainPage extends Component {
         <div className='mainpage_container_2'>
           <div className='playerArea'>
             <div className='playerArea_imageHolder right'>
-              <img className='playerImage' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
-              <img className='cardImage' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+              <PlayerImage
+              type='file'
+              playerID = '2'
+              imageUrl = 'https://placecage.com/gif/100/100'
+              />
+              <img className='cardImage' src={"https://placecage.com/gif/700/800"}/>
             </div>
             <div className='playerArea_nameHolder left' id='player2Name'>
               <div className='playerArea_nameHolder_name'>Test</div>
 
               <div className='chipArea'>
                 <div className='chip'>
-                  <img  id='p2Chip1' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                  <img  id='p2Chip1' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
                 <div className='chip'>
-                  <img  id='p2Chip2' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                  <img  id='p2Chip2' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
                 <div className='chip'>
-                  <img  id='p2Chip3' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                  <img  id='p2Chip3' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
               </div>
@@ -66,23 +75,27 @@ class MainPage extends Component {
         <div className='mainpage_container_3'>
           <div className='playerArea'>
             <div className='playerArea_imageHolder lower'>
-              <img className='playerImage' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
-              <img className='cardImage' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+              <PlayerImage
+              type='file'
+              playerID = '3'
+              imageUrl = 'https://placecage.com/gif/300/300'
+              />
+              <img className='cardImage' src={"https://placecage.com/gif/700/800"}/>
             </div>
             <div className='playerArea_nameHolder' id='player3Name'>
               <div className='playerArea_nameHolder_name right'>Test</div>
 
               <div className='chipArea'>
-                <div className='chip'>
-                  <img  id='p3Chip1' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                <div className='chip bottom'>
+                  <img  id='p3Chip1' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
-                <div className='chip'>
-                  <img  id='p3Chip2' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                <div className='chip bottom'>
+                  <img  id='p3Chip2' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
-                <div className='chip'>
-                  <img  id='p3Chip3' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                <div className='chip bottom'>
+                  <img  id='p3Chip3' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
               </div>
@@ -93,23 +106,27 @@ class MainPage extends Component {
         <div className='mainpage_container_4'>
           <div className='playerArea'>
             <div className='playerArea_imageHolder right lower '>
-              <img className='playerImage' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
-              <img className='cardImage' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+              <PlayerImage
+              type='file'
+              playerID = '4'
+              imageUrl = 'https://placecage.com/gif/500/800'
+              />
+            <img className='cardImage' src={"https://placecage.com/gif/700/800"}/>
             </div>
             <div className='playerArea_nameHolder' id='player4Name'>
               <div className='playerArea_nameHolder_name right'>Test</div>
 
-              <div className='chipArea'>
-                <div className='chip'>
-                  <img  id='p4Chip1' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+              <div className='chipArea bottom'>
+                <div className='chip bottom'>
+                  <img  id='p4Chip1' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
-                <div className='chip'>
-                  <img  id='p4Chip2' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                <div className='chip bottom'>
+                  <img  id='p4Chip2' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
-                <div className='chip'>
-                  <img  id='p4Chip3' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+                <div className='chip bottom'>
+                  <img  id='p4Chip3' src={"https://placecage.com/gif/700/800"}/>
                   <button>Roll</button>
                 </div>
               </div>
@@ -122,7 +139,7 @@ class MainPage extends Component {
       <div className='mainpage_container_middle'>
         <div className='roulette'>
           <div className='roulette_image_holder'>
-            <img className='roulette_image_holder_image' src={"https://s-media-cache-ak0.pinimg.com/originals/87/a0/da/87a0da65851a3abe203929e26fececb4.png"}/>
+            <img className='roulette_image_holder_image' src={"https://placecage.com/gif/700/800"}/>
           </div>
           <div className='roulette_type_holder'>
             <button>Roll</button>
