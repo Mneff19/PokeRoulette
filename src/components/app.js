4023b53c2669fe1e18d6e5c72b5c6cfe0de2f8f2
student <mpneff19@gmail.com>
2018-08-06 08:37:40 -0600

    fixed firefox/chrome issue

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import MainPage from './mainpage';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <MainPage/>
+      </div>
+    );
+  }
+}
